d05fbe2b9bce449d35c8d03d20b5cc3f54308f5a
DeltaOpenSource <datadccenter@gmail.com>
2025-12-13 23:57:00 +0900

    Update statistics.js

--- frontend/statistics.js
+++ frontend/statistics.js
@@ -1,3 +1,6 @@
 document.getElementById('cart').addEventListener('click', () => {
           window.location.href = 'index.html';
 });
+
+let cartItems = JSON.parse(localStorage.getItem("EatToday")) || [];
+console.log(cartItems)
